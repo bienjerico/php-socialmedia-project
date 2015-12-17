f315c4a86c25c2a6dcd23bb75b16709ddc9d2ff6
Bien Jerico Cueto <bien.jerico@gmail.com>
2015-12-17 17:24:24 +0800

    database

--- phpprojectdb.sql
+++ phpprojectdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2015 at 05:09 AM
--- Server version: 5.6.16
--- PHP Version: 5.5.9
+-- Generation Time: Dec 17, 2015 at 10:23 AM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -28,8 +28,9 @@ USE `phpprojectdb`;
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `emailaddress` varchar(50) DEFAULT NULL COMMENT 'username',
   `password` varchar(255) NOT NULL,
   `name` varchar(50) NOT NULL,
@@ -39,10 +40,10 @@ CREATE TABLE IF NOT EXISTS `users` (
   `address` varchar(255) NOT NULL,
   `mobilenumber` varchar(11) NOT NULL,
   `is_admin` int(1) NOT NULL COMMENT '0 - guest , 1 - admin ',
+  `picture_location` text NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 -- --------------------------------------------------------
 
@@ -50,17 +51,74 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `users_friends`
 --
 
+DROP TABLE IF EXISTS `users_friends`;
 CREATE TABLE IF NOT EXISTS `users_friends` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `friend_id` int(11) NOT NULL,
   `status` int(11) NOT NULL COMMENT '0 - pending , 1 - accepted',
   `requested_at` datetime NOT NULL,
   `accepted_at` datetime NOT NULL,
-  `created_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_messenger`
+--
+
+DROP TABLE IF EXISTS `users_messenger`;
+CREATE TABLE IF NOT EXISTS `users_messenger` (
+`id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `view_tag` int(1) NOT NULL COMMENT '0 - new , 1 - viewed',
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `users_friends`
+--
+ALTER TABLE `users_friends`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_messenger`
+--
+ALTER TABLE `users_messenger`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `users_friends`
+--
+ALTER TABLE `users_friends`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT for table `users_messenger`
+--
+ALTER TABLE `users_messenger`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=43;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
